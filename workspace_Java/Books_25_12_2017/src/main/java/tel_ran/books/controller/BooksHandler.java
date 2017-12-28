3a0f5350cf72b3da995176b7c2a75987b21773d6
Frickazoid <veden91@gmail.com>
2017-12-28 12:43:26 +0200

    add posgres heroku sittings

--- workspace_Java/Books_25_12_2017/src/main/java/tel_ran/books/controller/BooksHandler.java
+++ workspace_Java/Books_25_12_2017/src/main/java/tel_ran/books/controller/BooksHandler.java
@@ -14,36 +14,43 @@ public class BooksHandler {
     @Autowired
     IBooks books;
 
+    @CrossOrigin
     @PostMapping(Const.BOOK)
     public boolean addBook(@RequestBody Book book) {
         return books.addBook(book);
     }
 
+    @CrossOrigin
     @GetMapping({Const.BOOK + "/{isbn}"})
     public Book getBook(@PathVariable long isbn) {
         return books.getBookByISBN(isbn);
     }
 
+    @CrossOrigin
     @DeleteMapping({Const.BOOK + "/{isbn}"})
     public boolean removeBook(long isbn) {
         return books.removeBook(isbn);
     }
 
+    @CrossOrigin
     @GetMapping({Const.AUTHOR + "/{name}"})
     public Iterable<Book> booksByAuthor(@PathVariable String name) {
         return books.getBooksByAuthor(name);
     }
 
+    @CrossOrigin
     @GetMapping({Const.PUBLISHER + "/{name}"})
     public Iterable<Book> booksByPublisher(@PathVariable String name) {
         return books.getBooksByPublisher(name);
     }
 
+    @CrossOrigin
     @GetMapping({Const.AUTHORS + "/{isbn}"})
     public Iterable<Author> getAuthors(@PathVariable long isbn) {
         return books.getAuthorsByBook(isbn);
     }
 
+    @CrossOrigin
     @GetMapping({Const.PUBLISHERS + "/{name}"})
     public Iterable<Publisher> getPublishers(@PathVariable String name) {
         return books.getPublishersByAuthor(name);
